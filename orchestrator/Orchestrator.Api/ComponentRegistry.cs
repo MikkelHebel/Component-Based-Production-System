32f10a25f539caadd9bb2ee3e4dcc57ae8170d39
MikkelHebel <mikkelhebelv2@gmail.com>
2026-04-22 11:59:28 +0200

    Started work on the component registration

---
+++ orchestrator/Orchestrator.Api/ComponentRegistry.cs
@@ -0,0 +1,35 @@
+using Orchestrator.Core;
+namespace Orchestrator.Api;
+
+public class ComponentRegistry
+{
+    private Dictionary<string, IComponent> _components;
+    private readonly ILogger<ComponentRegistry> _logger;
+
+    public ComponentRegistry(ILogger<ComponentRegistry> logger) {
+        _logger = logger;
+        _components = new Dictionary<string, IComponent>();
+    }
+
+    public void Register(IComponent component) {
+        if (_components.ContainsKey(component.ComponentId)) {
+            _logger.LogWarning($"Component {component.ComponentId} already registered.");
+            return;
+        }
+        _components[component.ComponentId] = component;
+        _logger.LogInformation($"Component {component.ComponentId} has been added to registry");
+    }
+
+    public void DeRegister(IComponent component) {
+        if (!_components.ContainsKey(component.ComponentId)) {
+            _logger.LogWarning($"Component {component.ComponentId} is not registered");
+            return;
+        }
+        _components.Remove(component.ComponentId);
+        _logger.LogInformation($"Component {component.ComponentId} has been removed from registry");
+    }
+
+    public IComponent FindAvailable(string type) {
+        return _components.Values.FirstOrDefault(c => c.ComponentType == type);
+    }
+}
