9c1e75941155f859bc53876ecfa8a9542976a97d
MikkelHebel <mikkelhebelv2@gmail.com>
2026-04-22 11:34:15 +0200

    Setup Orchestrator.Core classes needed for later

---
+++ orchestrator/Orchestrator.Core/IComponent.cs
@@ -0,0 +1,11 @@
+﻿namespace Orchestrator.Core;
+
+public interface IComponent
+{
+    public string ComponentType { get; }
+    public string ComponentId { get; }
+    public string Protocol { get; }
+    public string Host { get; }
+    public int Port { get; }
+    public List<CommandDefinition> SupportedCommands { get; }
+}
