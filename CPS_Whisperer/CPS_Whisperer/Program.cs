5b327df07c434904663e44adf7db4ea29646ea09
MikkelHebel <mikkelhebelv2@gmail.com>
2026-05-04 15:50:59 +0200

    Setup Orchestrator and CPS_Whisperer in docker

--- CPS_Whisperer/CPS_Whisperer/Program.cs
+++ CPS_Whisperer/CPS_Whisperer/Program.cs
@@ -1,35 +1,5 @@
-using System;
-
-class Program
-{
-    static async Task Main(string[] args) {
-
-        Console.WriteLine("  AGV");
-        await AGV.Instance.Command("MoveToAssemblyOperation.1");
-
-        //------------------------
-        Console.WriteLine("\n  Warehouse");
-        await Warehouse.Instance.Command("1");
-        await Warehouse.Instance.Command("1.Item 1");
-        await Warehouse.Instance.CheckInventory();
-
-        //------------------------
-        Console.WriteLine("\n  Assembly station");
-        await AssemblyStation.Instance.Connect();
-        await AssemblyStation.Instance.Subscribe();
-
-        Console.WriteLine("Write ProcessID: ");
-        int processID = Convert.ToInt32(Console.ReadLine());
-        await AssemblyStation.Instance.Command(processID);
-        
-        while (true) {
-            Console.ReadLine(); 
-            Console.WriteLine("AGV: " + AGV.Instance.Status());
-            Console.WriteLine("Battery: " + AGV.Instance.battery + "%");
-            Console.WriteLine("WAREHOUSE: " + Warehouse.Instance.Status());
-            Console.WriteLine("ASSEMBLY STATION: " + AssemblyStation.Instance.Status());
-            Console.WriteLine("ASSEMBLY STATION HEALTH: " + AssemblyStation.Instance.CheckHealth());
-        }
-    }
-}
-
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+WebApplication app = builder.Build();
+app.MapControllers();
+app.Run();
